2cc777f3f45357f8944baf336e560e599850e448
Jakub Mikołajek <wiecny@laptop-Jakub.local>
2024-08-10 17:25:47 +0200

    feat: adding more stuff

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
+
 resource "proxmox_vm_qemu" "my_vm" {
   name        = "test-vm"
   target_node = "wiecny"
+  vmid        = "104"
+  clone       = "ubuntu-tempalte-v2"
+  full_clone  = true
+  os_type     = "cloud-init"
+  
   network {
-    model = "virtio"
+    model  = "virtio"
+    bridge = "vmbr0"
   }
+  ipconfig0 = "ip=192.168.1.201/24,gw=192.168.1.1"
 }
